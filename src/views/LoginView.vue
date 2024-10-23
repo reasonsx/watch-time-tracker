a2a7a874bd02ccfe55938c253c9f2902888278c6
reasonsx <kamkre01@easv365.dk>
2024-10-23 23:45:17 +0200

    added search input

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -41,7 +41,7 @@
         <div>
           <button
             type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            class="focus:ring-4 focus:outline-none focus:ring-indigo-700 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Sign in
           </button>
