a85def4d83ad5c3946420fa45e8283d81f13f967
reasonsx <kamkre01@easv365.dk>
2024-10-24 00:12:49 +0200

    update

--- src/components/home/MovieSearch.vue
+++ src/components/home/MovieSearch.vue
@@ -4,6 +4,7 @@
     <form class="px-10">
       <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
       <div class="flex items-center">
+        
         <div class="relative w-full">
           <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
--- src/components/home/TimeCount.vue
+++ src/components/home/TimeCount.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white p-6 fixed w-full z-10 shadow-md border border-gray-200">
     <h2 class="text-2xl font-bold mb-2 text-center text-indigo-600">Total Time</h2>
-    <p class="text-lg text-gray-700 text-center">{{ totalTime }} minutes</p>
+    <p class="text-lg text-gray-700 text-center">{{ formattedTotalTime }}</p>
     <p class="mt-4 text-sm text-gray-500 text-center">
       Click on movie posters to add time!
     </p>
@@ -9,9 +9,18 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const totalTime = ref(0);
+
+// Computed property to format total time
+const formattedTotalTime = computed(() => {
+  const days = Math.floor(totalTime.value / (60 * 24));
+  const hours = Math.floor((totalTime.value % (60 * 24)) / 60);
+  const minutes = totalTime.value % 60;
+
+  return `${days} days, ${hours} hours, ${minutes} minutes`;
+});
 
 // Function to add time to the total
 const addTime = (time) => {
