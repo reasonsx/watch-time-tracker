bcf3efb3838c671769aebd888546d40115bc21b9
reasonsx <kamkre01@easv365.dk>
2024-10-23 20:41:58 +0200

    Update MovieSearch.vue

--- src/components/home/MovieSearch.vue
+++ src/components/home/MovieSearch.vue
@@ -1,45 +1,37 @@
 <template>
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-
-      </div>
-      
-      <div class="mx-auto max-w-2xl sm:py-48 lg:py-16 mt-20">
-        
-<form>   
-    <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+  <div class="relative isolate px-6 pt-14 lg:px-8"></div>
+  
+  <div class="mx-auto max-w-2xl sm:py-48 lg:py-16 mt-20">
+    <form class="px-10">
+      <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <div class="flex items-center">
+        <div class="relative w-full">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
+          </div>
+          <input 
+            type="search" 
+            id="search" 
+            class="block w-full text-sm ps-10 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+            placeholder="Search" 
+            required 
+          />
         </div>
-        <input type="search" id="search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
-        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
-</form>
+        <button 
+          type="submit" 
+          class="ml-2 text-white bg-indigo-600 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-indigo-700 font-medium rounded-lg text-sm px-4 py-2"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
 
-        <div class="text-center">
-       
-          <p class="mt-6 text-lg leading-8 text-gray-600">Just type your favorite movie title below</p>
-          
-          <div class="relative mt-2 rounded-md shadow-sm">
-            <input 
-              type="text" 
-              name="movie-name" 
-              id="movie-name"
-              class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Movie name"
-            />
-          </div>
-          </div>
-        </div>
-      
-     
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  </style>
-  
+<script setup>
+</script>
+
+<style scoped>
+</style>
