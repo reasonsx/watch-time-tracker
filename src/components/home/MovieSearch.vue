efa9349a13ad44ad285bfb49ba34ade52faf1b87
reasonsx <kamkre01@easv365.dk>
2024-10-23 10:56:10 +0200

    Update MovieSearch.vue

--- src/components/home/MovieSearch.vue
+++ src/components/home/MovieSearch.vue
@@ -1,9 +1,23 @@
 <template>
-    <!-- <div class="relative isolate px-6 pt-14 lg:px-8">
+    <div class="relative isolate px-6 pt-14 lg:px-8">
 
       </div>
       
-      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div class="mx-auto max-w-2xl sm:py-48 lg:py-16 mt-20">
+        
+<form>   
+    <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    </div>
+</form>
+
         <div class="text-center">
        
           <p class="mt-6 text-lg leading-8 text-gray-600">Just type your favorite movie title below</p>
@@ -17,10 +31,10 @@
               placeholder="Movie name"
             />
           </div>
+          </div>
         </div>
       
      
-    </div> -->
   </template>
   
   <script setup>
