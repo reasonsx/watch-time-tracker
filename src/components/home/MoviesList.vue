a1d5574edefc222442ca90d49461ba575bc1895c
reasonsx <kamkre01@easv365.dk>
2024-10-28 23:40:52 +0100

    fixing bugs

--- src/components/home/MoviesList.vue
+++ src/components/home/MoviesList.vue
@@ -35,7 +35,7 @@
       </div>
 
       <!-- Movie Grid -->
-      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div class="grid grid-cols-1 gap-x-6 gap-y-10 max-sm:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         <div 
           v-for="movie in filteredMovies" 
           :key="movie.id" 
--- src/components/home/TimeCount.vue
+++ src/components/home/TimeCount.vue
@@ -4,10 +4,9 @@
     
     <!-- Total Time and Reset SVG -->
     <div class="flex items-center justify-center space-x-2">
-      <p class="text-lg text-gray-700">{{ formattedTotalTime }}</p>
       <svg 
         @click="resetTime" 
-        class="w-6 h-6 text-gray-600 cursor-pointer hover:text-gray-800 transition-colors duration-200 ease-in-out" 
+        class="w-6 h-6 text-gray-700 cursor-pointer hover:text-gray-700 transition-colors duration-200 ease-in-out" 
         aria-hidden="true" 
         xmlns="http://www.w3.org/2000/svg" 
         width="24" 
@@ -17,6 +16,7 @@
       >
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/>
       </svg>
+      <p class="text-lg text-gray-700">{{ formattedTotalTime }}</p>
     </div>
 
     <p class="mt-4 text-sm text-gray-500 text-center">
