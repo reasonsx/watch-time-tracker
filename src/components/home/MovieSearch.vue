4dca912a9b07865fbfdd46f93e8c1e1fb9cacc1e
reasonsx <kamkre01@easv365.dk>
2024-11-01 14:27:19 +0100

    ok

--- src/components/home/MovieSearch.vue
+++ src/components/home/MovieSearch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed inset-x-0 mt-20 z-10">
+  <div class="fixed inset-x-0 mt-28 z-10">
     <div class="mx-auto max-w-2xl pt-1 pb-8">
       <form class="px-10">
         <div class="relative w-full">
